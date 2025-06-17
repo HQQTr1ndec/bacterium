efa6bdc62194300521c9270274bde4b037ef2896
HQQTr1ndec <7evennns4@gmail.com>
2025-06-17 20:22:52 +0300

    add client window interface

--- client.py
+++ client.py
@@ -1,9 +1,108 @@
+import math
 import socket
-
+import pygame
 
 
 sock = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
 sock.setsockopt(socket.IPPROTO_TCP,socket.TCP_NODELAY,1)
 sock.connect(("localhost",10000))
-while True:
-    sock.send("Привет".encode())
+
+
+radius = 50
+old = (0,0)
+pygame.init()
+WIGTH = 800
+HEIGHT = 600
+CC = (WIGTH//2,HEIGHT//2)
+screen = pygame.display.set_mode((HEIGHT,WIGTH))
+pygame.display.set_caption("Бактерии")
+
+
+
+run = True
+while run:
+    for event in pygame.event.get():
+        if event == pygame.QUIT:
+            run = False
+    if pygame.mouse.get_focused():
+        pos = pygame.mouse.get_pos()
+        vector = pos[0]-CC[0],pos[1]-CC[1]
+
+        lenv = math.sqrt(vector[0]**2 + vector[1]**2)
+        vector = vector[0] / lenv,vector[1] / lenv
+        if lenv <= radius:
+            vector = 0, 0
+
+        if vector != old:
+            old = vector
+            msg = f"<{vector[0]},{vector[1]}>"
+            sock.send(msg.encode())
+            # sock.send("Стихи Пушкина - https://www.culture.ru/literature/poems/author-aleksandr-pushkin?ysclid=mbqqfikkbt274040913".encode())
+
+    screen.fill("gray")
+    pygame.draw.circle(screen,(255,0,0),CC,radius=radius)
+    pygame.display.update()
+
+    data = sock.recv(1024).decode()
+    print("получил ",data)
+pygame.quit()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- server.py
+++ server.py
@@ -1,5 +1,55 @@
 import time
 import socket
+import psycopg2
+from sqlalchemy import create_engine
+from sqlalchemy import Column,Integer,String
+from sqlalchemy.orm import declarative_base,sessionmaker
+
+
+
+
+engine = create_engine("postgresql+psycopg2://postgres:07021984Apple@localhost/Forse")
+Base = declarative_base()
+Session = sessionmaker(bind=engine)
+s = Session()
+
+class Players(Base):
+    __tablename__ = "gamers"
+    id = Column(Integer,primary_key=True,autoincrement=True)
+    name =Column(String(250))
+    adress = Column(String)
+    x = Column(Integer,default=500)
+    y = Column(Integer,default=500)
+    size = Column(Integer,default=50)
+    errors = Column(Integer,default=0)
+    abs_speed = Column(Integer,default=1)
+    speed_x = Column(Integer,default=0)
+    speed_y = Column(Integer,default=0)
+
+    def __init__(self,name,adress):
+        self.name = name
+        self.adress = adress
+
+
+class LocalPlayer:
+    def __init__(self,id,name,sock,addr):
+        self.id = id
+        self.name = name
+        self.sock = sock
+        self.address = addr
+        self.db:Players = s.get(Players,self.id)
+        self.x = 500
+        self.y = 500
+        self.errors = 0
+        self.abs_speed = 1
+        self.speed_x = 0
+        self.speed_y = 0
+
+
+
+
+
+
 
 main_socket = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
 main_socket.setsockopt(socket.IPPROTO_TCP,socket.TCP_NODELAY,1)
@@ -8,8 +58,8 @@ main_socket.setblocking(False)
 main_socket.listen(5)
 print("сокет создан")
 
-players = []
-
+# players = []
+players = {}
 while True:
     try:
         new_socket,addr = main_socket.accept()
@@ -22,7 +72,11 @@ while True:
         try:
             data = sock.recv(1024).decode()
             print("получил",data)
+            sock.send("LoL".encode())
 
         except:
-            pass
+            players.remove(sock)
+            sock.close()
+            print("сокет закрыт")
+
     time.sleep(2)
